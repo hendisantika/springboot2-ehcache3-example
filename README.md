ea7d5eaed36cb0e891b11d3c9c7815fcc8852ba2
hendisantika <hendisantika@yahoo.co.id>
2019-07-21 06:24:11 +0700

    #1 - Initial project

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot2-ehcache3-example
---
+++ src/main/java/com/hendisantika/springboot2ehcache3example/Springboot2Ehcache3ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot2ehcache3example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2Ehcache3ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2Ehcache3ExampleApplication.class, args);
+    }
+
+}
